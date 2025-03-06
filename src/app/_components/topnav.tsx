275b2b5278b8d83332b189d7cb5bc79dbd7eacdc
igleson <igleson.freire@vtex.com.br>
2025-03-06 15:29:39 -0300

    user cleck user button when logged

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,17 +1,24 @@
-import {SignedIn, SignedOut, SignInButton, SignOutButton} from "@clerk/nextjs";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 export function TopNav() {
-    return (
-        <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-            <div>Criar Nova Lista</div>
-            <div className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 rounded-lg px-5 py-2.5 text-center me-2 mb-2">
-                <SignedOut>
-                    <SignInButton>Logar</SignInButton>
-                </SignedOut>
-                <SignedIn>
-                    <SignOutButton>Sair</SignOutButton>
-                </SignedIn>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Criar Nova Lista</div>
+
+      <SignedOut>
+        <div className="mb-2 me-2 rounded-lg bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 px-5 py-2.5 text-center text-white hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-purple-300 dark:focus:ring-purple-800">
+          <SignInButton>Logar</SignInButton>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <UserButton></UserButton>
+      </SignedIn>
+    </nav>
+  );
 }
